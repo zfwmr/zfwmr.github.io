d4f4c66f5fe56f86dd0ee20829f89ee70d03ab25
frank0718 <gacia_0718@163.com>
2016-11-28 12:10:33 +0800

    add

--- cp.sh
+++ cp.sh
@@ -5,3 +5,16 @@ git checkout source
 git branch 
 cp -r ../hugo-blog/* ./
 rm -rf /Users/didi/Downloads/github_repo/zfwmr.github.io/public
+
+chown -R didi:staff /Users/didi/Downloads/github_repo/zfwmr.github.io
+
+
+
+#cd hugo-blog; 
+#hugo new post/hugo_blog_process.md
+#hugo undraft content/post/hugo_blog_process.md -v
+#sudo sh cp.sh
+#
+#cd zfwmr.github.io ; gitadd ; ignore error
+#travis-ci will compile when source branch pushed
+#https://travis-ci.org/zfwmr/zfwmr.github.io
